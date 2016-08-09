f2bc8c9f22836d2b372ca907e7764cd943efa10a
hutt <jannis@hutt.io>
2016-08-09 18:03:47 +0200

    added _s

---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.com/
+ *
+ * @package EUlia
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function eulia_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'eulia_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'eulia_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function eulia_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
