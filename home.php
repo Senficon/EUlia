0d8bb5ef6684e6085479aec3c78134c31067ea5a
hutt <jannis@hutt.io>
2016-08-09 18:19:01 +0200

    added home.php template

---
+++ home.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The first site being displayed when loading Julia's website.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package EUlia
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text">Hello World, especially c3o! <?php //single_post_title(); ?></h1>
+				</header>
+
+			<?php
+			endif;
+
+			/* Show the last post */
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', get_post_format() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
