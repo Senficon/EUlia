35cefec7de270a9b76c04554f95516b1a4fbd4f8
hutt <jannis@hutt.io>
2016-08-12 16:25:20 +0200

    modified comments.php

--- comments.php
+++ comments.php
@@ -23,25 +23,24 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
 	<?php
-	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
 				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'eulia' ) ),
+					esc_html( _nx( 'One question on &ldquo;%2$s&rdquo;', '%1$s questions on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'eulia' ) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			?>
 		</h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there questions to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'eulia' ); ?></h2>
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Question section navigation', 'eulia' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'eulia' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'eulia' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Questions', 'eulia' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Questions', 'eulia' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
@@ -61,8 +60,8 @@ if ( post_password_required() ) {
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'eulia' ); ?></h2>
 			<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'eulia' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'eulia' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Questions', 'eulia' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Questions', 'eulia' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
@@ -75,7 +74,7 @@ if ( post_password_required() ) {
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
 
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'eulia' ); ?></p>
+		<p class="no-comments"><?php esc_html_e( 'Questions aren\'t enabled.', 'eulia' ); ?></p>
 	<?php
 	endif;
 
