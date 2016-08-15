202b13ec8c98dccf7fe31e9906540e547939900b
hutt <jannis@hutt.io>
2016-08-15 16:09:17 +0200

    enqueued twitter.js

--- functions.php
+++ functions.php
@@ -104,9 +104,11 @@ add_action( 'widgets_init', 'eulia_widgets_init' );
 function eulia_scripts() {
 	wp_enqueue_style( 'eulia-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'eulia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'eulia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20160815', true );
 
-	wp_enqueue_script( 'eulia-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'eulia-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20160815', true );
+
+	wp_enqueue_script( 'eulia-twitter', get_template_directory_uri() . '/js/twitter.js', array(), '20160815', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
